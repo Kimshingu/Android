d418979fec917082338d61202922dcbc98ff49c2
KIM Shingu <creation07@naver.com>
2018-11-02 17:42:15 +0900

    18-11-02

---
+++ HelloAAR/app/src/main/java/com/example/administrator/helloaar/SayHello.java
@@ -0,0 +1,5 @@
+package com.example.administrator.helloaar;
+
+public interface SayHello {
+    String sayHello(String name);
+}
---
+++ HelloAAR/app/src/main/java/com/example/administrator/helloaar/SayHelloimpl.java
@@ -0,0 +1,7 @@
+package com.example.administrator.helloaar;
+
+public class SayHelloimpl implements  SayHello{
+    public String sayHello(String name){
+        return "Hello~" + name;
+    }
+}
